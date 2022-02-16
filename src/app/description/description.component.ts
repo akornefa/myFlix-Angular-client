f3fbb0fc317354fcb1da2a3ffcacb7d849b22356
Akornefa Akyea <64564297+akornefa@users.noreply.github.com>
2022-02-15 23:58:16 -0600

    Create description component and fetch data

---
+++ src/app/description/description.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-description',
+  templateUrl: './description.component.html',
+  styleUrls: ['./description.component.scss']
+})
+export class DescriptionComponent implements OnInit {
+
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { description: string }
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
