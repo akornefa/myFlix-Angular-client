95bf5c7c675d8137e1529850877587d7c24e295f
Akornefa Akyea <64564297+akornefa@users.noreply.github.com>
2022-02-15 23:55:09 -0600

    Create Navigation Menu

---
+++ src/app/navigation/navigation.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { MatDialog } from '@angular/material/dialog';
+import { UserProfileComponent } from '../user-profile/user-profile.component';
+import { MatMenuModule } from '@angular/material/menu';
+
+@Component({
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
+})
+export class NavigationComponent implements OnInit {
+
+  constructor(
+    public dialog: MatDialog,
+    public snackBar: MatSnackBar,
+    public menu: MatMenuModule,
+    public router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  movies(): void {
+    this.router.navigate(['movies']);
+  }
+
+  goToProfile(): void {
+    this.dialog.open(UserProfileComponent, {
+      width: '500px'
+    });
+  }
+
+  // goToFavorites(): void {
+  //   this.dialog.open(FavoritesComponent, {
+  //     width: '100%'
+  //   } );
+  // }
+
+  userLogout(): void {
+    localStorage.clear();
+    this.snackBar.open('You have successfully logged out!', 'OK', {
+      duration: 2000,
+    });
+    this.router.navigate(['welcome']).then(() => {
+      window.location.reload();
+    });
+  }
+
+}
